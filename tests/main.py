74746aa3b68242defabda41d00aff9a240ef9bd9
Jack Jiang <jack.jiang@harman.com>
2026-02-01 11:13:03 +0800

    update theme

--- tests/main.py
+++ tests/main.py
@@ -14,4 +14,7 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
+def dino(a, b, c):
+    return (-b + cmath.sqrt(b**2 - 4*a*c)) / (2*a)
+
 print('The solution are {0} and {1}'.format(sol1,sol2))
